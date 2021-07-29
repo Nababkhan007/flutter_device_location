4853ebc39926012ea02a1ec5b573623c88ca5a15
Nababkhan007 <markohenry46@gmail.com>
2021-07-29 15:25:07 +0600

    initial design added

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,51 @@ class _DeviceLocationState extends State<DeviceLocation> {
             ),
           ),
         ),
-        body: Container(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Get device location",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Icon(
+                Icons.location_on,
+                size: 22.0,
+                color: Colors.red,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: _getDeviceLocation(),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    Colors.red,
+                  ),
+                ),
+                child: Text(
+                  "Tap Here",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+
+  _getDeviceLocation() {}
 }
