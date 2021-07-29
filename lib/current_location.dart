87e7a2098a78b4035464487416aa53f600c772e3
Nababkhan007 <markohenry46@gmail.com>
2021-07-29 23:42:16 +0600

    curren location dart class added

---
+++ lib/current_location.dart
@@ -0,0 +1,31 @@
+import 'package:location/location.dart';
+
+class CurrentLocation {
+  static Future<dynamic> getDeviceLocation() async {
+    Location _location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionStatus;
+    LocationData _locationData;
+
+    _serviceEnabled = await _location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionStatus = await _location.hasPermission();
+    if (_permissionStatus == PermissionStatus.denied) {
+      _permissionStatus = await _location.requestPermission();
+      if (_permissionStatus != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await _location.getLocation();
+
+    return _locationData;
+  }
+}
