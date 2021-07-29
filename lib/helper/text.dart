a22f8d767647f797782238337f95beb19fafec96
Nababkhan007 <markohenry46@gmail.com>
2021-07-30 01:14:16 +0600

    added text dart file

---
+++ lib/helper/text.dart
@@ -0,0 +1,8 @@
+class TextHelper {
+  static const String DEVICE_LOCATION = "Device Location",
+      ADDRESS = "Address",
+      LATITUDE = "Latitude",
+      LONGITUDE = "Longitude",
+      GET_DEVICE_LOCATION = "Get device location",
+      TAP_HERE = "Tap Here";
+}
